d6e4729538390537ea15b90ab783ad7ca82cfd7b
Brey Rivera <breyr2021@gmail.com>
2018-10-10 18:48:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,12 +58,11 @@ bot.on("message", function(message){
                 break;
         case "lamp":
                 var embedLamp = new Discord.RichEmbed()
-                    .setFooter("Lamp")
+                    .setTitle("**BrÖther, pass the Lamp**")
                     .setColor(0xF9E79F)
                     .setImage("https://imgix.bustle.com/uploads/image/2018/9/27/b2655359-f3c4-436e-b7dc-7ceb3d528dfd-dj92z5rvsaa7uku.jpg?w=970&h=582&fit=crop&crop=faces&auto=format&q=70")
                 var i;
                      for (i = 1; i < 6; i++) { 
-                         //message.channel.sendMessage("Moth " + i + ": Lamp");
                          message.channel.sendEmbed(embedLamp);
                      }
                 break;
