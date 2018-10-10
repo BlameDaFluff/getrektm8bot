d2bb87c535bee781047fec37d1fa88c41a559aa6
Brey Rivera <breyr2021@gmail.com>
2018-10-10 18:25:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,11 +58,11 @@ bot.on("message", function(message){
                 break;
         case "lamp":
                 var i;
-                for (i = 1; i < 11; i++) { 
-                    setInterval(function() {
-                        message.channel.sendMessage("Moth " + i + ": Lamp");
-                    }, 600);
-                }
+                setInterval(function() {
+                        for (i = 0; i < 6; i++) { 
+                            message.channel.sendMessage("Moth " + i + ": Lamp");
+                        }
+                }, 5000);
                 break;
         case "commands":
             var embed = new Discord.RichEmbed()
